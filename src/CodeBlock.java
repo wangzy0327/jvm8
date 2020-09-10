70592182d37edcb7cded695f558b5512950d931a
wangzy0327 <wangzy0327@qq.com>
2020-09-10 10:21:35 +0800

    add inherit code block

--- src/CodeBlock.java
+++ src/CodeBlock.java
@@ -1,4 +1,3 @@
-
 class CodeZY{
     public CodeZY(){
         System.out.println("Code的构造方法1111");
@@ -8,6 +7,31 @@ class CodeZY{
     }
     static {
         System.out.println("Code的静态代码块3333");
+    }
+    static void printHello(){
+        System.out.println("Code的静态方法4444");
+    }
+}
+class Parent{
+    public Parent(){
+        System.out.println("Parent的构造方法1111");
+    }
+    {
+        System.out.println("Parent的构造块2222");
+    }
+    static {
+        System.out.println("Parent的静态代码块3333");
+    }
+}
+class Son extends Parent{
+    public Son(){
+        System.out.println("Son的构造方法11111");
+    }
+    {
+        System.out.println("Son的构造块22222");
+    }
+    static {
+        System.out.println("Son的静态代码块33333");
     }
 }
 public class CodeBlock { //主类  CodeBlock.class --- main
@@ -23,25 +47,44 @@ public class CodeBlock { //主类  CodeBlock.class --- main
 
     public static void main(String[] args) {
         System.out.println("--我是美丽分割线-----------CodeBlock03的main方法777");
-        new CodeZY();
-        System.out.println("------------------------------");
-        new CodeZY();
-        System.out.println("------------------------------");
-        new CodeBlock();
+
+        new Son();
+
+        /***
+         CodeBlock03的静态代码块555
+         --我是美丽分割线-----------CodeBlock03的main方法777
+         Parent的静态代码块3333
+         Son的静态代码块33333
+         Parent的构造块2222
+         Parent的构造方法1111
+         Son的构造块22222
+         Son的构造方法11111
+
+         */
+
+//        CodeZY.printHello();
+//        System.out.println("------------------------------");
+//        new CodeZY();
+//        System.out.println("------------------------------");
+//        new CodeZY();
+//        System.out.println("------------------------------");
+//        new CodeBlock();
 
         /**
          *
-         CodeBlock的静态代码块555
+         CodeBlock03的静态代码块555
          --我是美丽分割线-----------CodeBlock03的main方法777
          Code的静态代码块3333
-         Code的构造块2222
-         Code的构造方法1111
+         Code的静态方法4444
          ------------------------------
          Code的构造块2222
          Code的构造方法1111
          ------------------------------
-         CodeBlock的构造块444
-         CodeBlock的构造方法666
+         Code的构造块2222
+         Code的构造方法1111
+         ------------------------------
+         CodeBlock03的构造块444
+         CodeBlock03的构造方法666
          *
          */
     }
